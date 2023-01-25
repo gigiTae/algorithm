01560566721b2ce08820775f5315c269ed8a8c81
GiTae <rlxo800@gmail.com>
2023-01-26 02:41:11 +0900

    문자열

---
+++ Studyalgorithm/Studyalgorithm/ASCII.cpp
@@ -0,0 +1,12 @@
+//#include <iostream>
+//
+//
+//int main(void)
+//{
+//	char ASCII = 0;
+//	std::cin >> ASCII;
+//	std::cout << (int)ASCII;
+//
+//
+//	return 0;
+//}
--- Studyalgorithm/Studyalgorithm/main.cpp
+++ Studyalgorithm/Studyalgorithm/main.cpp
@@ -3,6 +3,14 @@
 
 int main(void)
 {
+	std::wstring str;
+	std::wcin >> str;
+	
+	int world = 0;
+
+	for (int i = 0; i < str.size(); ++i)
+	{
+	}
 
 	return 0;
 }
