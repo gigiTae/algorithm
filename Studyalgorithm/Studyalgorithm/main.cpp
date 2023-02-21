c55fa2f79593908f56d68ff454b5aca1759d629d
GiTae <rlxo800@gmail.com>
2023-02-21 20:57:19 +0900

    1로 만들기

--- Studyalgorithm/Studyalgorithm/main.cpp
+++ Studyalgorithm/Studyalgorithm/main.cpp
@@ -1,24 +1,51 @@
 
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
+int arr[1000001] = { 0, 0, 0, 0};
+
 int main()
 {
-	string str;
-	cin >> str;
-	int ans = 1;
-	for (int i = 0; i < str.size() / 2; ++i)
+	int num = 0;
+	cin >> num;
+	vector<int> vec;
+	vec.push_back(1);
+	int i = 1;
+	if (num != 1)
 	{
-		if (str[i] != *(str.end() - 1 - i))
+		while (arr[num] == 0)
 		{
-			ans = 0;
-			break;
+			vector<int> vec2;
+			for (auto iter = vec.begin(); iter != vec.end(); ++iter)
+			{
+				int X = *iter;
+				if (2 * X < 1000001 &&arr[2 * X] == 0)
+				{
+					arr[2 * X] = i;
+					vec2.push_back(2 * X);
+				}
+				if (3 * X < 1000001 && arr[3 * X] == 0)
+				{
+					arr[3 * X] = i;
+					vec2.push_back(3 * X);
+				}
+				if (X + 1 < 1000001 && arr[X + 1] == 0)
+				{
+					arr[X + 1] = i;
+					vec2.push_back(X + 1);
+				}
+			}
+			vec.clear();
+			vec = vec2;
+			++i;
 		}
 	}
 
-	cout << ans;
+	cout << arr[num];
 
+	return 0;
 }
 
 
