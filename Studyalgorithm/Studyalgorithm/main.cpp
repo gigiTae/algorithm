fcd6e2cb65fbd9480bafb1549b626904a7d350e7
GiTae <rlxo800@gmail.com>
2023-02-23 13:53:30 +0900

    누적합 3

--- Studyalgorithm/Studyalgorithm/main.cpp
+++ Studyalgorithm/Studyalgorithm/main.cpp
@@ -3,7 +3,40 @@
 
 using namespace std;
 
+
 int main()
 {
+	cin.tie(NULL);
+	cout.tie(NULL);
+	ios::sync_with_stdio(false);
 
+	int N, M;
+	cin >> N >> M;
+
+	long long* Remain = new long long[M];
+
+	for (int i = 0; i < M; ++i)
+	{
+		Remain[i] = 0;
+	}
+	Remain[0] = 1;
+	int PrevRemain = 0;
+	long long ans = 0;
+	for (int i = 0; i < N; ++i)
+	{
+		int num = 0;
+		cin >> num;
+		num = num % M;
+		PrevRemain = (num + PrevRemain) % M;
+		++Remain[PrevRemain];
+	}
+
+	for (int i = 0; i < M; ++i)
+	{
+		if (Remain[i] > 1)
+			ans += Remain[i] * (Remain[i] - 1) / 2;
+	}
+
+	cout << ans;
+	return 0;
 }
