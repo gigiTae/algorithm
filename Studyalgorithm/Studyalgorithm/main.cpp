8114f1db8f7fd5082e1615844523a83b2ba7c04a
GiTae <rlxo800@gmail.com>
2023-02-25 23:03:41 +0900

    제로

--- Studyalgorithm/Studyalgorithm/main.cpp
+++ Studyalgorithm/Studyalgorithm/main.cpp
@@ -1,97 +1,37 @@
 
 #include<iostream>
-
+#include<stack>
 using namespace std;
-
-class stack
-{
-private :
-	int arr[10000];
-	int end;
-	int capacity;
-public:
-	void push(int num) 
-	{
-		arr[end] = num;
-		++end;
-		++capacity;
-	}
-
-	int pop()
-	{
-		if (capacity == 0)
-			return -1;
-		else
-		{
-			--capacity;
-			return arr[--end];
-		}
-	}
-
-	int size()
-	{
-		return capacity;
-	}
-
-	int empty()
-	{
-		if (capacity == 0)
-			return 1;
-		else
-			return 0;
-	}
-
-	int top()
-	{
-		if (capacity == 0)
-			return -1;
-		else
-			return arr[end-1];
-	}
-
-	stack()
-		:end(0)
-		,capacity(0)
-		,arr{}
-	{
-
-	}
-};
 
 
 int main()
 {
-	stack S;
+	stack<int> S;
+	int K;
+	cin >> K;
 
-	int TestCase;
-	cin >> TestCase;
-	for (int i = 0; i < TestCase; ++i)
+	for (int i = 0; i < K; ++i)
 	{
-		string str;
-		cin >> str;
-		if (str == "push")
-		{
-			int num = 0;
-			cin >> num;
+		long long num;
+		cin >> num;
+		if (num == 0)
+			S.pop();
+		else
 			S.push(num);
-		}
-		else if (str == "pop")
-		{
-			cout << S.pop() << '\n';
-		}
-		else if (str == "size")
-		{
-			cout << S.size() << '\n';
-		}
-		else if (str == "empty")
-		{
-			cout << S.empty() << '\n';
-		}
-		else if (str == "top")
-		{
-			cout << S.top() << '\n';
-		}
 	}
 
+	long long ans = 0;
+
+	while (!S.empty())
+	{
+		ans += S.top();
+		S.pop();
+	}
+
+	cout << ans;
+
+
+
 	return 0;
+
 }
