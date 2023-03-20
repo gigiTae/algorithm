babbc87b330b53155db30cd33b3c6705374fa0e1
GiTae <rlxo800@gmail.com>
2023-03-20 23:39:28 +0900

    zd

--- Studyalgorithm/Studyalgorithm/main.cpp
+++ Studyalgorithm/Studyalgorithm/main.cpp
@@ -1,61 +1,19 @@
 ﻿
 #include <iostream>
-#include <queue>
+#include <set>
 using namespace std;
-int N, K;
-int ans = 0;
-queue<int> Q;
-bool Visited[100001]{};
-
-void BFS()
-{
-	queue<int> tmp;
-	while (!Q.empty())
-	{
-		int num = Q.front();
-		while (num < 100001)
-		{
-			Visited[num] == true;
-			if (num == K)
-			{
-				queue<int> emp;
-				Q = emp;
-				return;
-			}
-			if (Visited[num] == false)
-			{
-				Visited[num] == true;
-				tmp.push(num);
-			}
-			if (Visited[num + 1] == false)
-			{
-				tmp.push(num + 1);
-				Visited[num + 1] = true;
-			}
-			if (num > 0 && Visited[num - 1] == false)
-			{
-				 tmp.push(num - 1);
-				 Visited[num - 1] = true;
-			}
-
-			num *= 2;
-		}
-		Q.pop();
-	}
-	++ans;
-	Q = tmp;
-}
 
 int main()
 {
-	cin >> N >> K;
-	Q.push(N);
-
-	
-	Visited[N] = true;
-	while (!Q.empty())
+	int N, x;
+	multiset<int> S;
+	cin >> N;
+	for (int i = 0; i < N; ++i)
 	{
-		BFS();
+		int num;
+		cin >> num;
+		S.insert(num);
 	}
-	cout << ans;
+	cin >> x;
+
 }
